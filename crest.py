83ce0b0e59354ca0ba08c81005c301bc438c27a5
Camto <www.ben.pr@gmail.com>
2025-03-04 00:23:00 -0400

    Split execution of commands and expressions.

--- crest.py
+++ crest.py
@@ -19,11 +19,11 @@ with open(sys.argv[1]) as file:
 	prog = file.read()
 
 grammar = lark.Lark(r"""
-	start: stmt*
+	start: cmd*
 	
-	stmt: ARITY_0_STMT
-		| ARITY_1_STMT expr
-		| ARITY_2_STMT expr expr
+	cmd: ARITY_0_CMD
+		| ARITY_1_CMD expr
+		| ARITY_2_CMD expr expr
 		
 		| "if" expr block -> if_
 		| "ifelse" expr block block -> ifelse
@@ -36,9 +36,9 @@ grammar = lark.Lark(r"""
 		| ARITY_1_EXPR expr
 		| ARITY_2_EXPR expr expr
 	
-	ARITY_0_STMT: /pendown|penup|home|clean|clearscreen|showturtle|hideturtle|nextframe/
-	ARITY_1_STMT: /forward|back|left|right|setpencolor|setheading|debug/
-	ARITY_2_STMT: /setpos/
+	ARITY_0_CMD: /pendown|penup|home|clean|clearscreen|showturtle|hideturtle|nextframe/
+	ARITY_1_CMD: /forward|back|left|right|setpencolor|setheading|debug/
+	ARITY_2_CMD: /setpos/
 	
 	ARITY_0_EXPR: /heading|pixel|xcor|ycor|pendownp|pencolor|shownp|true|false/
 	ARITY_1_EXPR: /not/
@@ -47,7 +47,7 @@ grammar = lark.Lark(r"""
 	?number: SIGNED_FLOAT -> float
 		| SIGNED_INT -> int
 	
-	block: LEFT_SQUARE_BRACKET stmt* RIGHT_SQUARE_BRACKET
+	block: LEFT_SQUARE_BRACKET cmd* RIGHT_SQUARE_BRACKET
 	LEFT_SQUARE_BRACKET: "["
 	RIGHT_SQUARE_BRACKET: "]"
 	
@@ -59,7 +59,7 @@ grammar = lark.Lark(r"""
 
 class transformer(lark.Transformer):
 	start = lambda self, ls: list(ls)
-	stmt = start
+	cmd = start
 	expr = start
 	block = start
 	if_ = lambda self, ast: ["if"] + ast
@@ -100,10 +100,9 @@ window_height = 600
 window = (window_width, window_height)
 
 fps_opts = [int(s[4:]) for s in options if s.startswith("fps=")]
-if not fps_opts:
-	fps = 30
-else:
-	fps = fps_opts[0]
+fps = 30 if not fps_opts else fps_opts[0]
+
+debug = "debug" in options
 
 turtle_pos = (window_width / 2, window_height / 2)
 turtle_angle = 0
@@ -112,141 +111,11 @@ pen_color = color_to_rgb(0)
 is_pen_down = True
 next_frame = False
 
-def run_instr(instr):
-	global turtle_pos
-	global turtle_angle
-	global is_turtle_shown
-	global pen_color
-	global is_pen_down
-	global next_frame
-	
-	instr_name = instr[0]
-	if (
-			isinstance(instr_name, int)
-			or isinstance(instr_name, float)):
-		return instr_name
-	elif instr_name == "forward":
-		dist = run_instr(instr[1])
-		rad = deg_to_rad(turtle_angle)
-		new_turtle_pos = (
-			turtle_pos[0] + math.cos(rad) * dist,
-			turtle_pos[1] + math.sin(rad) * dist)
-		move_to(new_turtle_pos)
-	elif instr_name == "back":
-		dist = -run_instr(instr[1])
-		rad = deg_to_rad(turtle_angle)
-		new_turtle_pos = (
-			turtle_pos[0] + math.cos(rad) * dist,
-			turtle_pos[1] + math.sin(rad) * dist)
-		move_to(new_turtle_pos)
-	elif instr_name == "left":
-		turtle_angle -= run_instr(instr[1])
-	elif instr_name == "right":
-		turtle_angle += run_instr(instr[1])
-	elif instr_name == "pendown":
-		is_pen_down = True
-	elif instr_name == "penup":
-		is_pen_down = False
-	elif instr_name == "setpencolor":
-		pen_color = color_to_rgb(round(run_instr(instr[1])) % 600)
-	
-	elif instr_name == "setpos":
-		move_to((run_instr(instr[1]), run_instr(instr[2])))
-	elif instr_name == "xcor":
-		return turtle_pos[0]
-	elif instr_name == "ycor":
-		return turtle_pos[1]
-	elif instr_name == "setheading":
-		turtle_angle = run_instr(instr[1])
-	elif instr_name == "heading":
-		return turtle_angle
-	elif instr_name == "pixel":
-		return rgb_to_color(memory.get_at(round_pos(turtle_pos)))
-	elif instr_name == "pendownp":
-		return int(is_pen_down)
-	elif instr_name == "pencolor":
-		return rgb_to_color(pen_color)
-	elif instr_name == "home":
-		turtle_pos = (window_width / 2, window_height / 2)
-		turtle_angle = 0
-	elif instr_name == "clean":
-		memory.fill(white)
-	elif instr_name == "clearscreen":
-		turtle_pos = (window_width / 2, window_height / 2)
-		turtle_angle = 0
-		memory.fill(white)
-	
-	elif instr_name == "showturtle":
-		is_turtle_shown = True
-	elif instr_name == "hideturtle":
-		is_turtle_shown = False
-	elif instr_name == "shownp":
-		return int(is_turtle_shown)
-	
-	elif instr_name == "true":
-		return 1
-	elif instr_name == "false":
-		return 0
-	elif instr_name == "and":
-		return int(run_instr(instr[1]) and run_instr(instr[2]))
-	elif instr_name == "or":
-		return int(run_instr(instr[1]) or run_instr(instr[2]))
-	elif instr_name == "not":
-		return int(not run_instr(instr[1]))
-	elif instr_name == "equal":
-		return int(run_instr(instr[1]) == run_instr(instr[2]))
-	elif instr_name == "lessthan":
-		return int(run_instr(instr[1]) < run_instr(instr[2]))
-	elif instr_name == "morethan":
-		return int(run_instr(instr[1]) > run_instr(instr[2]))
-	elif instr_name == "plus":
-		return run_instr(instr[1]) + run_instr(instr[2])
-	elif instr_name == "minus":
-		return run_instr(instr[1]) - run_instr(instr[2])
-	
-	elif instr_name == "if":
-		def gen():
-			if run_instr(instr[1]):
-				for _ in run_block(instr[2]):
-					yield
-		return gen()
-	elif instr_name == "ifelse":
-		def gen():
-			branch = instr[2] if run_instr(instr[1]) else instr[3]
-			for _ in run_block(branch):
-				yield
-		return gen()
-	elif instr_name == "repeat":
-		def gen():
-			for _ in range(run_instr(instr[1])):
-				for _ in run_block(instr[2]):
-					yield
-		return gen()
-	elif instr_name == "forever":
-		def gen():
-			while True:
-				for _ in run_block(instr[1]):
-					yield
-		return gen()
-	elif instr_name == "while":
-		def gen():
-			while run_instr(instr[1]):
-				for _ in run_block(instr[2]):
-					yield
-		return gen()
-	
-	elif instr_name == "nextframe":
-		next_frame = True
-	
-	elif instr_name == "debug":
-		if "debug" in options:
-			print(run_instr(instr[1]))
-
 time_since_started_frame = 0
 
 def run_block(block):
-	for instr in block:
-		res = run_instr(instr)
+	for cmd in block:
+		res = run_cmd(cmd)
 		
 		if should_render():
 			yield
@@ -258,10 +127,146 @@ def run_block(block):
 	
 	yield
 
+def run_cmd(cmd):
+	global turtle_pos
+	global turtle_angle
+	global is_turtle_shown
+	global pen_color
+	global is_pen_down
+	global next_frame
+	
+	cmd_name = cmd[0]
+	# Turtle Commands
+	if cmd_name == "forward":
+		dist = run_expr(cmd[1])
+		rad = deg_to_rad(turtle_angle)
+		new_turtle_pos = (
+			turtle_pos[0] + math.cos(rad) * dist,
+			turtle_pos[1] + math.sin(rad) * dist)
+		move_to(new_turtle_pos)
+	elif cmd_name == "back":
+		dist = -run_expr(cmd[1])
+		rad = deg_to_rad(turtle_angle)
+		new_turtle_pos = (
+			turtle_pos[0] + math.cos(rad) * dist,
+			turtle_pos[1] + math.sin(rad) * dist)
+		move_to(new_turtle_pos)
+	elif cmd_name == "left":
+		turtle_angle -= run_expr(cmd[1])
+	elif cmd_name == "right":
+		turtle_angle += run_expr(cmd[1])
+	elif cmd_name == "setpos":
+		move_to((run_expr(cmd[1]), run_expr(cmd[2])))
+	elif cmd_name == "setheading":
+		turtle_angle = run_expr(cmd[1])
+	elif cmd_name == "showturtle":
+		is_turtle_shown = True
+	elif cmd_name == "hideturtle":
+		is_turtle_shown = False
+	# Pen Commands
+	elif cmd_name == "setpencolor":
+		pen_color = color_to_rgb(round(run_expr(cmd[1])) % 600)
+	elif cmd_name == "penup":
+		is_pen_down = False
+	elif cmd_name == "pendown":
+		is_pen_down = True
+	# Reset Commands
+	elif cmd_name == "home":
+		turtle_pos = (window_width / 2, window_height / 2)
+		turtle_angle = 0
+	elif cmd_name == "clean":
+		memory.fill(white)
+	elif cmd_name == "clearscreen":
+		turtle_pos = (window_width / 2, window_height / 2)
+		turtle_angle = 0
+		memory.fill(white)
+	# Control Flow Commands
+	elif cmd_name == "if":
+		def gen():
+			if run_expr(cmd[1]):
+				for _ in run_block(cmd[2]):
+					yield
+		return gen()
+	elif cmd_name == "ifelse":
+		def gen():
+			branch = cmd[2] if run_expr(cmd[1]) else cmd[3]
+			for _ in run_block(branch):
+				yield
+		return gen()
+	elif cmd_name == "repeat":
+		def gen():
+			for _ in range(run_expr(cmd[1])):
+				for _ in run_block(cmd[2]):
+					yield
+		return gen()
+	elif cmd_name == "forever":
+		def gen():
+			while True:
+				for _ in run_block(cmd[1]):
+					yield
+		return gen()
+	elif cmd_name == "while":
+		def gen():
+			while run_expr(cmd[1]):
+				for _ in run_block(cmd[2]):
+					yield
+		return gen()
+	# Time Commands
+	elif cmd_name == "nextframe":
+		next_frame = True
+	# SeCrEt CoMmAnDs OoOoOo
+	elif cmd_name == "debug":
+		if debug:
+			print(run_expr(cmd[1]))
+
+def run_expr(expr):
+	expr_name = expr[0]
+	if (
+			isinstance(expr_name, int)
+			or isinstance(expr_name, float)):
+		return expr_name
+	# Turtle Expressions
+	elif expr_name == "xcor":
+		return turtle_pos[0]
+	elif expr_name == "ycor":
+		return turtle_pos[1]
+	elif expr_name == "heading":
+		return turtle_angle
+	elif expr_name == "pixel":
+		return rgb_to_color(memory.get_at(round_pos(turtle_pos)))
+	elif expr_name == "shownp":
+		return int(is_turtle_shown)
+	# Pen Expressions
+	elif expr_name == "pendownp":
+		return int(is_pen_down)
+	elif expr_name == "pencolor":
+		return rgb_to_color(pen_color)
+	# Math Expressions
+	elif expr_name == "true":
+		return 1
+	elif expr_name == "false":
+		return 0
+	elif expr_name == "and":
+		return int(run_expr(expr[1]) and run_expr(expr[2]))
+	elif expr_name == "or":
+		return int(run_expr(expr[1]) or run_expr(expr[2]))
+	elif expr_name == "not":
+		return int(not run_expr(expr[1]))
+	elif expr_name == "equal":
+		return int(run_expr(expr[1]) == run_expr(expr[2]))
+	elif expr_name == "lessthan":
+		return int(run_expr(expr[1]) < run_expr(expr[2]))
+	elif expr_name == "morethan":
+		return int(run_expr(expr[1]) > run_expr(expr[2]))
+	elif expr_name == "plus":
+		return run_expr(expr[1]) + run_expr(expr[2])
+	elif expr_name == "minus":
+		return run_expr(expr[1]) - run_expr(expr[2])
+
 def should_render():
 	return (
-		pygame.time.get_ticks() - time_since_started_frame >= 1000/fps or
-		next_frame)
+		pygame.time.get_ticks() - time_since_started_frame >= 1000/fps
+		or next_frame)
 
 def move_to(pos):
 	global turtle_pos
@@ -287,11 +292,7 @@ memory = pygame.Surface(window)
 memory.fill(white)
 
 turtle_opts = [s[7:] for s in options if s.startswith("turtle=")]
-if not turtle_opts:
-	turtle_file = "turtle.png"
-else:
-	turtle_file = turtle_opts[0]
-turtle = pygame.image.load(turtle_file)
+turtle = pygame.image.load("turtle.png" if not turtle_opts else turtle_opts[0])
 
 icon = pygame.image.load("icon.png")
 pygame.display.set_icon(icon)
@@ -328,7 +329,7 @@ while not done:
 			rotated_turtle = pygame.transform.rotate(turtle, -turtle_angle)
 			screen.blit(rotated_turtle, (
 				round(turtle_pos[0]) - rotated_turtle.get_width() / 2,
-				(turtle_pos[1]) - rotated_turtle.get_height() / 2))
+				round(turtle_pos[1]) - rotated_turtle.get_height() / 2))
 		
 		pygame.display.flip()
 	except StopIteration:
